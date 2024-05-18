f4921e3c4e016a2536304de765c5f50fa1a04ce8
Swep_0 <swep0986@gmail.com>
2024-05-18 12:32:31 +0200

    2024-05-18 12:32

---
+++ commit.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+datatime_now=`date "+%Y-%m-%d %H:%M"`
+git config --global user.name "Swep_0"
+git config --global user.email "swep0986@gmail.com"
+git add *
+git commit -m "$datatime_now"
+git branch -M main
+git remote add origin https://github.com/Swep0/test_repo_new.git
+git push -u origin main
