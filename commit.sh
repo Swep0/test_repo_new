f547f7d3e6d1fd1f45427c9f937633a1525057c0
Swep0 <swep0986@gmail.com>
2024-05-18 12:51:19 +0200

    2024-05-18 12:51

--- commit.sh
+++ commit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 datatime_now=`date "+%Y-%m-%d %H:%M"`
-git config --global user.name "Swep_0"
+git config --global user.name "Swep0"
 git config --global user.email "swep0986@gmail.com"
 git add *
 git commit -m "$datatime_now"
